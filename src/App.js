03fc20d97065410880da3dba09e3f35280d93de1
hirose_shigemoto <hirose_shigemoto@tenda.co.jp>
2020-06-14 21:25:28 +0900

    create functional component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+// function App() {
+//   // const greeting = 'Hia!!!'
+//   // const dom = <h1 className="foo">{greeting}</h1>
+//   const input = <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       {input}
+//     </React.Fragment>
+//   );
+// }
+
 function App() {
-  // const greeting = 'Hia!!!'
-  // const dom = <h1 className="foo">{greeting}</h1>
-  const input = <input type="text" onChange={() => {console.log("I am clicked.")}} />
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      {input}
-    </React.Fragment>
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+function Cat() {
+  return <div>Meow!!</div>
 }
 
 export default App;
