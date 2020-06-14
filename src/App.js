6525ebc784ac0b14ca5999bdae82755552ff82ad
hirose_shigemoto <hirose_shigemoto@tenda.co.jp>
2020-06-14 22:07:45 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-function App() {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 25},
-    {name: "NoName", age: 3},
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User key={index} name={profile.name} age={profile.age}/>
-        })
-      }
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButtion = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButtion = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  
+  render() {
+    return (
+      <>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButtion}>+1</button>
+        <button onClick={this.handleMinusButtion}>-1</button>
+      </>
+    )
+  }
 }
-
-function User(props) {
-  return <div>Hi, I am {props.name} and {props.age} years old!</div>
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
-
 
 export default App;
