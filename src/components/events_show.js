eeff2f5728d1ed5279370cfd7a62d873d231152f
hirose_shigemoto <superblueplanet@gmail.com>
2020-06-22 20:59:47 +0900

    apply material-ui to event new

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -21,10 +23,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} autoComplete="new-password" />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -41,6 +47,7 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -48,9 +55,22 @@ class EventsShow extends Component {
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
         <div>
-          <input type="submit" value="submit" disabled={pristine || submitting || invalid} />
-          <Link to="/">Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+          <RaisedButton
+            label="Submit"
+            type="submit"
+            style={style}
+            disabled={pristine || submitting || invalid}
+          />
+          <RaisedButton
+            label="Cancel"
+            style={style}
+            containerElement={<Link to="/" />}
+          />
+          <RaisedButton
+            label="Delete"
+            style={style}
+            onClick={this.onDeleteClick}
+          />
         </div>
       </form>
     )
@@ -68,7 +88,7 @@ const validate = values => {
 
 const mapStateToProps = (state, ownProps) => {
   const event = state.events[ownProps.match.params.id]
-  return { initialValues: event, event}
+  return { initialValues: event, event }
 }
 
 const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
